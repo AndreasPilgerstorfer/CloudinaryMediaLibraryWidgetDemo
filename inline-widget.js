aff1eb3cf04a10a6e10a4d794212f76449d488db
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2025-10-01 08:53:44 +0200

    Update inline-widget.js

--- inline-widget.js
+++ inline-widget.js
@@ -1,40 +1,19 @@
-document.getElementById("openWidget").addEventListener("click", () => {
-    const ml = cloudinary.createMediaLibrary(
-      {
-        cloud_name: cloudNameVD,
-        api_key: apiKeyVD,
-        multiple: true,
-        use_saml: true,
-      },
-      {
-        insertHandler: (data) => {
-          console.log("Selected Assets:", data.assets);
-          generateMediaEntities(data.assets);
-        },
-      },
-    );
-  
-    ml.show();
-  });
-  
+let mediaLibrary = cloudinary.createMediaLibrary({
+    cloud_name: cloudName,
+    api_key: apiKey,
+    use_saml: true,
+    inline_container: "#cloudinary_widget",
+    multiple: false,
+    // Open in Search Tab
+    default_tab: "search",
+},
+{
+  insertHandler: function(data) {
+    // Insert doesnt do anything
+    console.log("Unused Insert:", data);
+  }
+}
+);
 
-  let mediaLibrary = cloudinary.createMediaLibrary(
-    {
-        cloud_name: cloudNameVD,
-        api_key: apiKeyVD,
-        use_saml: true,
-        inline_container: "#cloudinary_widget",
-        multiple: false,
-        // Open in Search Tab
-        default_tab: "search",
-    },
-    {
-      insertHandler: function(data) {
-        // Insert doesnt do anything
-        console.log("Unused Insert:", data);
-      }
-    }
-  );
-
-  // Open Inline Widget
-  mediaLibrary.show();
+// Open Inline Widget
+mediaLibrary.show();
